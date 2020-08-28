03f85b9c708e7d579673ca4ccd63d79e2f856a84
su-ram <swamys0031@gmail.com>
2020-08-28 22:51:48 +0900

    중간 저장

--- DFS_BFS/BJ_1697.py
+++ DFS_BFS/BJ_1697.py
@@ -1,1 +1,31 @@
 #백준 1697 숨바꼭질
+
+from collections import deque
+
+#입력
+n, k = map(int, input().split())
+
+shortest = {}
+
+queue = deque()
+queue.append(n)
+
+cnt = 0
+root = []
+
+while queue:
+
+
+    node = queue.popleft()
+    root.append(node)
+
+    if node == k : break
+
+    queue.extend([node*2, node-1, node+1])
+    cnt += 1
+    shortest[node*2] = cnt
+    shortest[node-1] = cnt
+    shortest[node+1] = cnt
+    print(shortest)
+
+
