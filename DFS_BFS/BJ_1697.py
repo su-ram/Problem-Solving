1ca25ac8bd947b55482107f93185df1627ac68fb
su-ram <swamys0031@gmail.com>
2020-08-29 00:44:05 +0900

    아 모르겠다

--- DFS_BFS/BJ_1697.py
+++ DFS_BFS/BJ_1697.py
@@ -4,28 +4,3 @@ from collections import deque
 
 #입력
 n, k = map(int, input().split())
-
-shortest = {}
-
-queue = deque()
-queue.append(n)
-
-cnt = 0
-root = []
-
-while queue:
-
-
-    node = queue.popleft()
-    root.append(node)
-
-    if node == k : break
-
-    queue.extend([node*2, node-1, node+1])
-    cnt += 1
-    shortest[node*2] = cnt
-    shortest[node-1] = cnt
-    shortest[node+1] = cnt
-    print(shortest)
-
-
