7371097afad9152bc3518ae9a489cf84ef4e1ee1
su-ram <swamys0031@gmail.com>
2020-08-29 02:54:09 +0900

    재귀는 어려워

--- DFS_BFS/BJ_1697.py
+++ DFS_BFS/BJ_1697.py
@@ -7,9 +7,12 @@ n, k = map(int, input().split())
 depth = 0
 limit = k - n
 limit = 2**limit*n
+cnt = 0
 
 def sol(root):
-    print(root)
+    global cnt
+    print("my level is %d" % cnt)
+    cnt += 1
     if root == 0 or root == 1: return 1
     else: return sol(root-1) + sol(root-2)
 
