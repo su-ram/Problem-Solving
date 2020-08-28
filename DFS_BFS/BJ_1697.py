7a92207ec0e86b51606fae9401916efa6384de34
su-ram <swamys0031@gmail.com>
2020-08-29 02:50:10 +0900

    지금은 재귀 연습 중.....

--- DFS_BFS/BJ_1697.py
+++ DFS_BFS/BJ_1697.py
@@ -4,3 +4,13 @@ from collections import deque
 
 #입력
 n, k = map(int, input().split())
+depth = 0
+limit = k - n
+limit = 2**limit*n
+
+def sol(root):
+    print(root)
+    if root == 0 or root == 1: return 1
+    else: return sol(root-1) + sol(root-2)
+
+sol(n)
