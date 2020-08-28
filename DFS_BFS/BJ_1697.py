ea2c143c444e6d3b8b472e0d2a479130713d59de
su-ram <swamys0031@gmail.com>
2020-08-29 02:00:35 +0900

    You have to solve this using RECURSION!!!

--- DFS_BFS/BJ_1697.py
+++ DFS_BFS/BJ_1697.py
@@ -1,20 +1,6 @@
 #백준 1697 숨바꼭질
-
+#이거 재귀로 풀어야 해!!!!
 from collections import deque
 
 #입력
 n, k = map(int, input().split())
-q = deque()
-visited={}
-visited[n] = 0
-cnt = 0
-q.append(n)
-add = [0, 1, -1]
-while q :
-    node = q.popleft()
-    add[0] = node
-    cnt += 1
-    for i in range(3):
-        val = node + add[i]
-        visited[val] = cnt
-        q.append(val)
