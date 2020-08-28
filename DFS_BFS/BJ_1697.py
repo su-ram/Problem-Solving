429704a5a52375fd39be790baaf1fcdabec1922f
su-ram <swamys0031@gmail.com>
2020-08-29 01:48:24 +0900

    This is commit

--- DFS_BFS/BJ_1697.py
+++ DFS_BFS/BJ_1697.py
@@ -4,3 +4,17 @@ from collections import deque
 
 #입력
 n, k = map(int, input().split())
+q = deque()
+visited={}
+visited[n] = 0
+cnt = 0
+q.append(n)
+add = [0, 1, -1]
+while q :
+    node = q.popleft()
+    add[0] = node
+    cnt += 1
+    for i in range(3):
+        val = node + add[i]
+        visited[val] = cnt
+        q.append(val)
