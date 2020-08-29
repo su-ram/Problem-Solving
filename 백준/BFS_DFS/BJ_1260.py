6ad7d3151669edccb0fd32e68ece2a683310b85f
su-ram <swamys0031@gmail.com>
2020-08-30 00:53:21 +0900

    백준 폴더 정리2

---
+++ 백준/BFS_DFS/BJ_1260.py
@@ -0,0 +1,57 @@
+import sys
+from collections import deque
+
+nodes,edges,root=map(int,sys.stdin.readline().split())
+
+g=[[]]
+for _ in range(nodes):
+    g.append([])
+
+for _ in range(edges):
+    i,j=map(int,sys.stdin.readline().split())
+    g[i].append(j)
+    g[j].append(i)
+
+for i in g:
+    i.sort()
+
+
+def DFS(g,root):
+
+    visited=[]
+    stack=[root]
+
+    while stack:
+
+        node = stack.pop()
+        sis=g[node]
+        sis=reversed(sis)
+
+        if node not in visited:
+            visited.append(node)
+            print(node,end=' ')
+            stack.extend(sis)
+    return visited
+DFS(g,root)
+print()
+def BFS(g,root):
+
+    visited=[]
+    queue=deque([root])
+
+    while queue:
+
+        node = queue.popleft()
+        sis=g[node]
+
+
+        if node not in visited:
+            visited.append(node)
+            print(node,end=' ')
+            queue.extend(sis)
+
+    return visited
+BFS(g,root)
+
+
+
---
+++ 백준/BFS_DFS/BJ_7576.py
@@ -0,0 +1,69 @@
+from collections import deque
+print("I'm changed!")
+print("I'm gonna sleeeeep.")
+m,n = map(int,input().split())
+tmt = [list(map(int,input().split())) for _ in range(n)]
+visited = [[0]*m for _ in range(n)]
+
+def BFS(roots):
+
+    nx , ny = [0,0,-1,1], [1,-1,0,0]
+
+    queue = deque()
+    queue.extend(roots)
+
+    next=[]
+
+
+
+    while queue:
+
+
+        node = queue.popleft()
+
+
+        for k in range(4):
+
+            a, b = node[0] + nx[k], node[1] + ny[k]
+
+
+            if 0 <= a < n and 0 <= b < m :
+
+                if visited[a][b] == 0 and tmt[a][b] == 0 :
+                    tmt[a][b] = 1
+                    visited[a][b] = 1
+                    next.append((a,b))
+
+
+    return next
+
+def check():
+    for _ in tmt:
+        if 0 in _:
+            return False
+    return True
+
+roots=[]
+
+for i in range(n):
+    for j in range(m):
+
+        if tmt[i][j] == 1:
+            roots.append([(i,j)])
+
+cnt=0
+for i in range(n*m):
+    if check():
+        break
+    for k in range(len(roots)):
+        newroot=BFS(roots[k])
+        roots[k] = newroot
+    cnt+=1
+if cnt == n*m:
+    print(-1)
+else:
+    print(cnt)
+
+
+
+
