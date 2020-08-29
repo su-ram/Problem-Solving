efe54479f0436aea94733fb9d2d8948341aa93bc
su-ram <swamys0031@gmail.com>
2020-08-29 20:49:40 +0900

    Second Lowest

--- HackerRank/Nested_Lists.py
+++ HackerRank/Nested_Lists.py
@@ -1,3 +1,23 @@
+# second lowest grade.
+
+records = {}
+names = []
 for _ in range(int(input())):
     name = input()
     score = float(input())
+    if score in records.keys():
+        temp = records[score]
+        temp.append(name)
+        records[score] = temp
+    else:
+        records[score] = [name]
+lowest = min(records.keys())
+second = False
+for i in sorted(records.keys()):
+    if i > lowest and not second :
+        second = True
+        names.extend(records[i])
+
+
+for i in sorted(names):
+    print(i)
