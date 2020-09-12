6cddcfa84d074feb9a6f6eb5baa284134dc23e49
su-ram <swamys0031@gmail.com>
2020-09-13 02:56:03 +0900

    베이비진 문제 테스트케이스 통과하려고 수정해봤지만 계속 그대로..ㅠ

--- SWEA/Baby_Gin.py
+++ SWEA/Baby_Gin.py
@@ -1,6 +1,5 @@
 T = int(input())
 
-
 def isrun(arr):
     cnt = 0
     for i in arr:
@@ -12,12 +11,10 @@ def isrun(arr):
             cnt = 0
     return False
 
-
 def triplet(cnt):
     for i in cnt:
         if i >= 3: return True
     return False
-
 
 for i in range(T):
 
@@ -28,31 +25,22 @@ for i in range(T):
     cnt1 = [0] * 10
     cnt2 = [0] * 10
 
-    winner = []
-
-    for j in range(0, 12, 2):
-
-        a, b = num[j], num[j + 1]
-        print(a,b)
+    for j in range(6):
+        a, b = num[2*j+0], num[2*j + 1]
+        winner = []
         p1[a] = 1
         p2[b] = 1
         cnt1[a] += 1
         cnt2[b] += 1
-
         if j >= 2:
             if isrun(p1) or triplet(cnt1):
                 winner.append(1)
             if isrun(p2) or triplet(cnt2):
                 winner.append(2)
+
             if len(winner) == 1:
+                print("#%d %d" %(i+1, winner[0]))
                 break
-
-    winners = len(winner)
-    if winners == 0 or winners == 2:
-        winner = 0
-    else:
-        winner = winner[0]
-
-    print()
-
+    if len(winner) == 0 or len(winner) == 2:
+        print("#%d %d"%(i+1, 0))
 
