429783c0a1f732feaa38c60d55a9a319f05fe2db
su-ram <swamys@naver.com>
2020-09-23 19:12:13 +0900

    부모 찾기 인접리스트로 최종 해결, 그러나 재귀 호출 제한을 해야 통과할 수 있었다.

--- 백준/BFS_DFS/트리의 부모 찾기.py
+++ 백준/BFS_DFS/트리의 부모 찾기.py
@@ -1,31 +1,24 @@
 n = int(input())
-#그래프 표현 : 인접 행렬
+#그래프 표현 : 인접 리스트
 
-matrix = [[0]*(n+1) for _ in range(n+1)]
+matrix = [[] for _ in range(n+1)]
 
 for i in range(n-1):
     x, y = map(int, input().split())
-    matrix[x][y] = 1
-    matrix[y][x] = 1
+    matrix[x].append(y)
+    matrix[y].append(x)
 
 visited = [0]*(n+1)
 parent_list= [0]*(n+1)
 def DFS(root,parent):
-    parent_list[root] = parent
-    visited[root] = 1
 
-    connected = []
-    for i in range(1,n+1):
-        if matrix[root][i] == 1:
-            connected.append(i)
-    for i in connected:
+    visited[root] = parent
+    for i in matrix[root]:
         if visited[i] == 0 :
-            DFS(i, root)
-    return 1
+            DFS(i,root)
 
-
-DFS(1,0)
-for i in parent_list[2:]:
+DFS(1,-1)
+for i in visited[2:]:
     print(i)
 
 
