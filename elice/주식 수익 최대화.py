1e2be3aaf6360a7aaf1778ec4add445c812b5f22
suram <swamys@naver.com>
2021-03-20 02:09:23 +0900

    [엘리스] - 주식 수익 최대화 문제 dp로 해결

--- elice/주식 수익 최대화.py
+++ elice/주식 수익 최대화.py
@@ -1,26 +1,23 @@
 def maximizeProfit(nums):
+    # dp로 푸는 것인가?!!
 
-    buying = nums[0]
-    selling = -1
-    next_buying = -1
+    dp = [0] * len(nums)
+    dp[1] = nums[1] - nums[0] if nums[1] - nums[0] > 0 else 0
 
-    for n in nums:
+    lowest_selling_price = nums[0]
 
-        if n > selling:
-            selling = n
+    for i in range(2, len(nums)):
 
-            if next_buying != -1:
-                    buying = next_buying
-                    next_buying = -1
+        dp[i] = max(nums[i] - lowest_selling_price, dp[i - 1])
 
-        if n < buying:
-            next_buying = n
+        if nums[i] < lowest_selling_price:
+            lowest_selling_price = nums[i]
 
-    return selling - buying
+    return max(dp)
+
 
 def main():
-
-    print(maximizeProfit([7,6,5,4,3]))
+    print(maximizeProfit([3, 8, 1, 7, 3, 1, 2]))  # 35
 
 
 if __name__ == "__main__":
