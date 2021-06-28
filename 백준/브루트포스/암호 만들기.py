11791418725ae13a7cdc9b2a82aae99548d67907
suram <swamys@naver.com>
2021-06-28 19:24:39 +0900

    [브루트포스/DFS] 백준 암호만들기 DFS로 풀이

--- 백준/브루트포스/암호 만들기.py
+++ 백준/브루트포스/암호 만들기.py
@@ -1,26 +1,43 @@
 L, C = map(int, input().split())
 characters = list(input().split())
 vowels = set(['a', 'e', 'i', 'o', 'u'])
-from itertools import combinations
+from itertools import combinations, permutations
 
 characters.sort()
 candidates = combinations(characters,L)
 
-for c in candidates:
+# for c in candidates:
+#
+#     print(c)
+#     vowwl_check = vowels.intersection(c)
+#
+#     if len(vowwl_check) < 1 or len(vowwl_check) > L-2:
+#         continue
+#
+#     isordered = True
+#
+#     for i in range(L-1):
+#         if c[i+1] < c[i] :
+#             isordered = False
+#             break
+#
+#     if not isordered:
+#         continue
+#
+#     #print(''.join(c))
 
-    vowwl_check = vowels.intersection(c)
 
-    if len(vowwl_check) < 1 or len(vowwl_check) > L-2:
-        continue
+def DFS(index):
+    out.append(characters[index])
+    if len(out) == L :
+        num_vowles = vowels.intersection(out)
+        if 1 <= len(num_vowles) <= L - 2:
+            print(''.join(out))
+        return
+    for i in range(index+1, C):
+        DFS(i)
+        del out[-1]
 
-    isordered = True
-    for i in range(L-1):
-        if c[i+1] < c[i] :
-            isordered = False
-            break
-
-    if not isordered:
-        continue
-
-    print(''.join(c))
-
+for j in range(C):
+    out = []
+    DFS(j)
